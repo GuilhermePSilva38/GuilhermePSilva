50df28cfbfafc7ba23d0aa481f1f658b1e7e87e5
Guilherme P. Silva <227608412+GuilhermePSilva38@users.noreply.github.com>
2025-09-03 10:59:40 -0300

    My first cpp

---
+++ primeiro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y;
+    cout << "Digite um numero inteiro: ";
+    cin >> x;
+    cout << "Digite um segundo mumero inteiro: ";
+    cin >> y;
+    cout << "soma = " << x*y << "\n";
+    cout << "subtracao = " << x-y << "\n";    
+    cout << "multiplicacao = " << x*y << "\n";
+    cout << "divisao = " << z << "\n";
+    cout << "modulo = " << x%y << "\n";
+    return 0;
+}
